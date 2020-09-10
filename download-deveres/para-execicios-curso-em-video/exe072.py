a455a4b179cdc0cde9dc9be476a1a2fb3aaaade7
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2020-09-10 17:11:54 -0300

    eu me perdir na contagem!

--- download-deveres/para-execicios-curso-em-video/exe072.py
+++ download-deveres/para-execicios-curso-em-video/exe072.py
@@ -2,7 +2,7 @@ num = int(input('digite um numero entre 0 e 20 :'))
 if num < 0 or num > 20:
     while True:
         num = int(input('digite um numero :'))
-        if num > 0 and num < 20:
+        if num >= 0 and num <= 20:
             break
 extenco = ['zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze',
            'doze', 'trese', 'quatoze', 'quinze', 'deseceis', 'desesete', 'desoito', 'desenove', 'vinte']

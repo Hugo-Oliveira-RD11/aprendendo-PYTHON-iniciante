562d63694227b459ef7793539a6d115662916f14
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2020-09-11 16:23:34 -0300

    n termineir

---
+++ download-deveres/para-execicios-curso-em-video/exe076.py
@@ -0,0 +1,5 @@
+print('-'*30)
+print(f'{"LISTAGEM DE PREÇO":^30}')
+print('-'*30)
+lista = ('lapis',1.75,'borracha', 2,'caderno',15.90,'estojo',25,'transferidor',4.20,'compasso', 9.99,'mochila', 120.32,'canetas', 22.30,
+'livro',34.90)

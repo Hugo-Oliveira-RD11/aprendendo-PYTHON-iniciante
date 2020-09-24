d8e3063fae0cc55754bcf6b512df8c9707f0c27f
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2020-09-24 17:17:20 -0300

    teste2 fail

--- download-deveres/para-aulas-do-curso-em-video/aula20.py
+++ download-deveres/para-aulas-do-curso-em-video/aula20.py
@@ -1,5 +1,12 @@
-def mensagem(msg):
-    print('-'*22)
-    print(msg)
-    print('-'*22)
-mensagem('ola hugo como vai?')
+
+def contar(* n):
+    soma = 0
+    for c in n:
+        soma += c
+    print(soma)
+
+nun = int(input('quantos numeros vc quer somar :'))
+for c in range(0,nun):
+    nun2=int(input('digite os numeros para a soma :'))
+
+contar(nun2)
